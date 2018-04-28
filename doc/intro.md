dbfb681fd9fc2e83d4387c9e73afce9ba0926227
Unknown <valerauko@gmail.com>
2018-04-28 18:14:43 +0900

    lein new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to thefox
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/thefox/core_test.clj
@@ -0,0 +1,7 @@
+(ns thefox.core-test
+  (:require [clojure.test :refer :all]
+            [thefox.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
