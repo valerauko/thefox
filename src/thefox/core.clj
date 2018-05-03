d3720668233df843a94983bd10486b87e3c917a0
Unknown <valerauko@gmail.com>
2018-05-03 18:12:48 +0900

    Those are strings

--- src/thefox/core.clj
+++ src/thefox/core.clj
@@ -2,22 +2,23 @@
 
 (def activity-types
   "List of Activity types"
-  '(Accept Add Announce Arrive Block Create Delete Dislike Flag Follow Ignore
-    Invite Join Leave Like Listen Move Offer Question Reject Read Remove
-    TentativeAccept TentativeReject Travel Undo Update View))
+  '("Accept" "Add" "Announce" "Arrive" "Block" "Create" "Delete" "Dislike"
+    "Flag" "Follow" "Ignore" "Invite" "Join" "Leave" "Like" "Listen" "Move"
+    "Offer" "Question" "Reject" "Read" "Remove" "TentativeAccept"
+    "TentativeReject" "Travel" "Undo" "Update" "View"))
 
 (def actor-types
   "List of Actor types"
-  '(Application Group Organization Person Service))
+  '("Application" "Group" "Organization" "Person" "Service"))
 
 (def object-types
   "List of Object types"
-  '(Article Audio Document Event Image Note Page Place Profile Relationship
-    Tombstone Video))
+  '("Article" "Audio" "Document" "Event" "Image" "Note" "Page" "Place" "Profile"
+    "Relationship" "Tombstone" "Video"))
 
 (def link-types
   "List of Link types"
-  '(Link Mention))
+  '("Link" "Mention"))
 
 (def recipient-keys
   "List of keys that may hold recipients"
