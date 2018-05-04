e8e44dc4149e51b29d13fb8476245d0a7ecddece
Unknown <valerauko@gmail.com>
2018-05-04 21:00:58 +0900

    Simple Activity verbs

--- src/thefox/activity.clj
+++ src/thefox/activity.clj
@@ -11,13 +11,76 @@
         (select-keys activity recipient-keys)
         (if (coll? object) (select-keys object recipient-keys))))))
 
-(defn create
-  "Makes a Create Activity for an Object"
-  [object, options]
+; REVIEW: What to do with timestamps? Should we add published automatically?
+
+(defn make-activity
+  [type actor object options]
   (merge
-    { :type "Create"
-      :object object }
     (or (get object "@context") default-context)
-    (select-keys object recipient-keys)
-    (select-keys object [:published])
+    { :type type :actor actor :object object }
     options))
+
+(defn announce
+  [actor object options]
+  (make-activity "Announce" actor object options))
+
+; some more meaningful Announce aliases
+(def share announce)
+(def recommend announce)
+
+(defn create
+  [object options]
+  (make-activity "Create" (:attributedTo object) object
+    (merge (select-keys object recipient-keys)
+           options)))
+
+(defn update
+  [actor object options]
+  (make-activity "Update" actor object options))
+
+(defn delete
+  "The distinction between Delete and Remove is that Delete implies the
+  destruction of the Object, while Remove shows that it is no longer a
+  member of a Collection."
+  [actor object options]
+  (make-activity "Delete" actor object options)
+
+(defn undo
+  "Undo is basically Delete for Activities.
+  Please don't ask what should happen if you Undo an Undo."
+  [activity]
+  (make-activity "Undo"
+    (:actor activity)
+    (:id activity)
+    options))
+
+; Using upvote/downvote instead of the ActivityPub spec's Like/Dislike
+; because Dislike is prone to be mistaken for Undoing a Like, which it is not
+; intended to be. Upvote/downvote makes the distinction more obvious.
+; Like/Dislike are aliased.
+; https://www.w3.org/TR/activitystreams-vocabulary/#inverse
+(defn upvote
+  [actor object options]
+  (make-activity "Like" actor object options))
+
+(def like upvote)
+
+(defn downvote
+  [actor object options]
+  (make-activity "Dislike" actor object options))
+
+(def dislike downvote)
+
+(defn listen
+  [actor object options]
+  (make-activity "Listen" actor object options))
+
+(defn read
+  [actor object options]
+  (make-activity "Read" actor object options))
+
+(defn view
+  [actor object options]
+  (make-activity "View" actor object options))
+
+(def watch view)
