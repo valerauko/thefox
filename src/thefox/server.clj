d24dcd056f4c617e8a63209a7add9cefc62aa1d4
Unknown <valerauko@gmail.com>
2018-05-13 17:06:59 +0900

    Server function skeletons

--- src/thefox/server.clj
+++ src/thefox/server.clj
@@ -12,6 +12,25 @@
     ; TODO
     ))
 
+(defn local?
+  "Determines whether an Object is local based on its ID."
+  [object]
+  (or
+    ; if an Object has no ID, we consider it local
+    ; because it was probably just made
+    (nil? (:id object))
+    ; TODO: consider it local if no host in url (need an url lib)
+    ; TODO: consider if local if the host is the configured host (need to handle config)
+    ))
+
+(defn should-forward?
+  "Rules: https://www.w3.org/TR/activitypub/#inbox-forwarding
+  Assuming that you've already checked step 1 (whether this is the first time
+  the server sees this Activity)."
+  [activity]
+  ; TODO
+  )
+
 (defn lookup
   "Dereferences an object based on its ID/URI.
   If provided an URI, look that up.
