d6c48a6902ac7d76e2430054cacfd2d6ae7b0fbd
Unknown <valerauko@gmail.com>
2018-06-10 18:16:42 +0900

    Raise to Clojure 1.9

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.codec "0.1.1"]
                  [org.bouncycastle/bcprov-jdk16 "1.46"]
                  [cheshire "5.8.0"]
